2f9281459531bb64c896fe6823d6bd7cda6ca34d
VoPhong2503 <voquocphong6678@gmail.com>
2024-11-25 12:59:48 +0700

    them ham ghi log

--- dataWareHouse/src/ult/Logger.java
+++ dataWareHouse/src/ult/Logger.java
@@ -1,5 +1,23 @@
 package ult;
 
-public class Logger {
+import java.time.LocalDateTime;
 
+import static ult.DatabaseManager.saveLog;
+
+public class Logger {
+    private static final EmailNotifier emailNotifier = new EmailNotifier();
+    // Phương thức ghi log
+    public static void log(String message) {
+        // In ra console
+        System.out.println(LocalDateTime.now() + ": " + message);
+
+        // Lưu log vào database
+        boolean check = saveLog(message);
+
+        // Gửi email nếu là lỗi
+        if (!check) {
+            String errorMessage = "Lỗi khi ghi log vào cơ sở dữ liệu: " + message;
+            emailNotifier.sendErrorLog(errorMessage);
+        }
+    }
 }
