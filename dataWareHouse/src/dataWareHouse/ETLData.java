c50e25579c55c6d4dbe3c0c33eac0fc6566a6732
ThanhQuyen1 <21130508@st.hcmuaf.edu.vn>
2024-12-11 23:28:09 +0700

    fix: load data

--- dataWareHouse/src/dataWareHouse/ETLData.java
+++ dataWareHouse/src/dataWareHouse/ETLData.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 public class ETLData {
@@ -34,13 +36,13 @@ public class ETLData {
 		}
 	}
 
+	// 4.1 connect database
 	public Connection getConnect() {
 
 		Connection conn = null;
 		try {
 			// Kết nối đến SQL Server
 			return DriverManager.getConnection(url, username, password);
-//			System.out.println("Kết nối thành công!");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -52,29 +54,32 @@ public class ETLData {
 		ETLData etl = new ETLData();
 		Connection connection = null;
 		try {
-	        connection = etl.getConnect();
-	        String sql = "SET DATEFORMAT ymd";
-	        PreparedStatement pr = connection.prepareStatement(sql);
-	        pr.execute(); 
-	        return true;
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return false;
+			connection = etl.getConnect();
+			String sql = "SET DATEFORMAT dmy";
+			PreparedStatement pr = connection.prepareStatement(sql);
+			pr.execute();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 
 	}
 
-// load CrawData
-	public boolean loadCrawData(String Path) {
+// load CrawData staging
+	public boolean loadCrawData_Staging(String Path) {
 		ETLData etl = new ETLData();
-		Path = "D:/DataWareHouse/Data/Prize31102024.csv";
+		Path = "D:\\DataWareHouse\\Data\\ket_qua_xo_so_t8.csv";
 		Connection connection = null;
 		try {
 			connection = etl.getConnect();
-			String sql = "exec [dbo].[proc_addCrawData] ?;";
+			String sql = "exec [dbo].[proc_addCrawData_Staging] ?;";
 			PreparedStatement pr = connection.prepareStatement(sql);
-			 pr.execute(); 
-	        return true;
+			pr.setString(1, Path);
+			int rs = pr.executeUpdate();
+			if (rs > 0) {
+				return true;
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -84,6 +89,27 @@ public class ETLData {
 	}
 
 //	4. load Staging
+	public boolean loadCrawData() {
+		ETLData etl = new ETLData();
+		Connection connection = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			String formattedDate = sdf.format(new Date());
+			connection = etl.getConnect();
+			String sql = "exec [dbo].[proc_addCrawData]";
+			PreparedStatement pr = connection.prepareStatement(sql);
+			pr.execute();
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	
+//	4. load Staging
 	public boolean loadStaging() {
 		ETLData etl = new ETLData();
 		Connection connection = null;
@@ -91,8 +117,8 @@ public class ETLData {
 			connection = etl.getConnect();
 			String sql = "exec [dbo].[proc_addStagingPrize]";
 			PreparedStatement pr = connection.prepareStatement(sql);
-			 pr.execute(); 
-		        return true;
+			pr.execute();
+			return true;
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -109,8 +135,8 @@ public class ETLData {
 			connection = etl.getConnect();
 			String sql = "exec [dbo].[proc_addDimDate] ";
 			PreparedStatement pr = connection.prepareStatement(sql);
-			 pr.execute(); 
-		        return true;
+			pr.execute();
+			return true;
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -127,8 +153,8 @@ public class ETLData {
 			connection = etl.getConnect();
 			String sql = "exec [dbo].[proc_addDimLocation]";
 			PreparedStatement pr = connection.prepareStatement(sql);
-			 pr.execute(); 
-		        return true;
+			pr.execute();
+			return true;
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -145,8 +171,8 @@ public class ETLData {
 			connection = etl.getConnect();
 			String sql = "exec [dbo].[proc_addFactPrize]";
 			PreparedStatement pr = connection.prepareStatement(sql);
-			 pr.execute(); 
-		        return true;
+			pr.execute();
+			return true;
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -172,12 +198,11 @@ public class ETLData {
 //			// TODO: handle exception
 //		}
 //	}
-	
+
 	public static void main(String[] args) {
 		ETLData etl = new ETLData();
 		etl.getConnect();
-		
-		
+
 //		etl.test();
 		String path = etl.Path;
 		// set date format
@@ -187,48 +212,56 @@ public class ETLData {
 		} else {
 			System.err.println("Failed to set date format");
 		}
-		
+
 		// Load CrawData
 		System.out.println("Loading Craw Data...");
-		if (etl.loadCrawData(path)) {
-			System.out.println("Craw Data loaded successfully.");
+		if (etl.loadCrawData_Staging(path)) {
+			System.out.println("CrawData_Staging loaded successfully.");
 		} else {
-			System.err.println("Failed to load Craw Data.");
+			System.err.println("Failed to load CrawData_Staging.");
 		}
-//
-//		// Load Staging
-//		System.out.println("Loading Staging Data...");
-//		if (etl.loadStaging()) {
-//			System.out.println("Staging Data loaded successfully.");
-//		} else {
-//			System.err.println("Failed to load Staging Data.");
-//		}
-//
-//		// Load DimDate
-//		System.out.println("Loading DimDate...");
-//		if (etl.loadDimDate()) {
-//			System.out.println("DimDate loaded successfully.");
-//		} else {
-//			System.err.println("Failed to load DimDate.");
-//		}
-//
-//		// Load DimLocation
-//		System.out.println("Loading DimLocation...");
-//		if (etl.loadDimLocation()) {
-//			System.out.println("DimLocation loaded successfully.");
-//		} else {
-//			System.err.println("Failed to load DimLocation.");
-//		}
-//
-//		// Load FactPrize
-//		System.out.println("Loading FactPrize...");
-//		if (etl.loadFactPrize()) {
-//			System.out.println("FactPrize loaded successfully.");
-//		} else {
-//			System.err.println("Failed to load FactPrize.");
-//		}
-//
-//		System.out.println("ETL process completed.");
+
+		// Load craw data
+				System.out.println("Loading CrawData...");
+				if (etl.loadCrawData()) {
+					System.out.println("CrawData  loaded successfully.");
+				} else {
+					System.err.println("Failed to load CrawData.");
+				}
+		
+		// Load Staging
+		System.out.println("Loading Staging Data...");
+		if (etl.loadStaging()) {
+			System.out.println("Staging Data loaded successfully.");
+		} else {
+			System.err.println("Failed to load Staging Data.");
+		}
+
+		// Load DimDate
+		System.out.println("Loading DimDate...");
+		if (etl.loadDimDate()) {
+			System.out.println("DimDate loaded successfully.");
+		} else {
+			System.err.println("Failed to load DimDate.");
+		}
+
+		// Load DimLocation
+		System.out.println("Loading DimLocation...");
+		if (etl.loadDimLocation()) {
+			System.out.println("DimLocation loaded successfully.");
+		} else {
+			System.err.println("Failed to load DimLocation.");
+		}
+
+		// Load FactPrize
+		System.out.println("Loading FactPrize...");
+		if (etl.loadFactPrize()) {
+			System.out.println("FactPrize loaded successfully.");
+		} else {
+			System.err.println("Failed to load FactPrize.");
+		}
+
+		System.out.println("ETL process completed.");
 	}
 
 }
